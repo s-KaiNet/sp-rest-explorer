3ac8a666035f4ffa832047a34a4f5cbf23117e50
Sergei Sergeev <sergeev.srg@gmail.com>
2018-06-06 00:29:15 +0300

    Fixing github pages urls.

--- web/config/index.js
+++ web/config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../../docs'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/sp-rest-explorer/',
 
     /**
      * Source Maps
